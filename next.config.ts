2b7e58d2a7bbf7a449ee8b39695b4c047eae51ef
iamshivamishra <alwayshivamishra@gmail.com>
2026-06-15 21:05:25 +0530

    Update project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   allowedDevOrigins: ["192.168.0.101"],
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.101"],
+  
+  // ✅ YE ADD KAR - file upload ke liye zaroori hai
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
+  
   images: {
     remotePatterns: [
       {
